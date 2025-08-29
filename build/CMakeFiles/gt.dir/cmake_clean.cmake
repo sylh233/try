9aa18ffc154fcd0c02ba503cd211d008943b5328
Sylh <785168843@qq.com>
2025-08-29 14:43:12 +0800

    new cmake

---
+++ build/CMakeFiles/gt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gt.dir/try.cpp.obj"
+  "CMakeFiles/gt.dir/try.cpp.obj.d"
+  "Y:/Cpp_work/git_try/gt.exe"
+  "Y:/Cpp_work/git_try/gt.exe.manifest"
+  "Y:/Cpp_work/git_try/gt.pdb"
+  "Y:/Cpp_work/git_try/libgt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- try.cpp
+++ try.cpp
@@ -1,2 +1,7 @@
 #include<fstream>
 #include "CPP_H/tryh.h"
+
+int main()
+{
+    return 0;
+}
